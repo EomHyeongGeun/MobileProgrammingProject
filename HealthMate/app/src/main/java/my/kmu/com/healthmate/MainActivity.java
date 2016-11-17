d078e62e0013b087e7a7a397709e6d7d92599a4e
EomHyeongGeun <eom2390@naver.com>
2016-11-17 22:31:35 +0900

    2016/11/17 first

---
+++ HealthMate/app/src/main/java/my/kmu/com/healthmate/MainActivity.java
@@ -0,0 +1,17 @@
+package my.kmu.com.healthmate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}

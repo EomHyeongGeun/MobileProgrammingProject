8fd0ed70911cd6ebb7fe0e15928b4ec0ab3b3c2c
EomHyeongGeun <eom2390@naver.com>
2016-11-20 14:06:34 +0900

    2016/11/20 main layout

--- HealthMate/NavigationDrawer/src/main/java/my/kmu/com/navigationdrawer/MainActivity.java
+++ HealthMate/NavigationDrawer/src/main/java/my/kmu/com/navigationdrawer/MainActivity.java
@@ -1,17 +1,13 @@
-package my.kmu.com.healthmate;
+package my.kmu.com.navigationdrawer;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 }

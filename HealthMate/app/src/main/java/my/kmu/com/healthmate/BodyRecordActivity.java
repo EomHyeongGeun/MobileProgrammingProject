36b05fa4e703b772a4db35df50c2db117cb8848b
EomHyeongGeun <eom2390@naver.com>
2016-11-20 19:47:00 +0900

    link to each page in main layout

---
+++ HealthMate/app/src/main/java/my/kmu/com/healthmate/BodyRecordActivity.java
@@ -0,0 +1,16 @@
+package my.kmu.com.healthmate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by eomhyeong-geun on 2016. 11. 20..
+ */
+
+public class BodyRecordActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.body_record);
+    }
+}
--- HealthMate/app/src/main/java/my/kmu/com/healthmate/MainActivity.java
+++ HealthMate/app/src/main/java/my/kmu/com/healthmate/MainActivity.java
@@ -1,7 +1,9 @@
 package my.kmu.com.healthmate;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -14,4 +16,21 @@ public class MainActivity extends Activity {
 
     }
 
+    // 메인화면에 있는 4가지 레이아웃 클릭시 해당 액티비티로 이동
+    public void goToTimeTable(View v){
+        Intent intent = new Intent(getApplicationContext(), TimeTableActivity.class);
+        startActivity(intent);
+    }
+    public void goToHowToDo(View v){
+        Intent intent = new Intent(getApplicationContext(), HowToDoActivity.class);
+        startActivity(intent);
+    }
+    public void goToPedometer(View v){
+        Intent intent = new Intent(getApplicationContext(), PedometerActivity.class);
+        startActivity(intent);
+    }
+    public void goToBodyRecord(View v){
+        Intent intent = new Intent(getApplicationContext(), BodyRecordActivity.class);
+        startActivity(intent);
+    }
 }

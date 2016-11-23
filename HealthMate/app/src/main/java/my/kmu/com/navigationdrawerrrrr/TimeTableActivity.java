fdbcc737a61e6b31ae7417b23eeacab66ad38fcd
EomHyeongGeun <eom2390@naver.com>
2016-11-24 02:26:09 +0900

    add Navigation Drawer

---
+++ HealthMate/app/src/main/java/my/kmu/com/navigationdrawerrrrr/TimeTableActivity.java
@@ -0,0 +1,16 @@
+package my.kmu.com.navigationdrawerrrrr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by eomhyeong-geun on 2016. 11. 20..
+ */
+
+public class TimeTableActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_table);
+    }
+}

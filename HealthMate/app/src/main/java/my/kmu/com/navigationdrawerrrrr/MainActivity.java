e9e1eacc044da5fcd2304c54fb209c2ba0e4530c
EomHyeongGeun <eom2390@naver.com>
2016-11-25 22:57:32 +0900

    add Pedometer function

--- HealthMate/app/src/main/java/my/kmu/com/navigationdrawerrrrr/MainActivity.java
+++ HealthMate/app/src/main/java/my/kmu/com/navigationdrawerrrrr/MainActivity.java
@@ -57,7 +57,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_back) {
+            return true;
+        }
+        if (id == R.id.action_home) {
             return true;
         }
 
--- HealthMate/app/src/main/java/my/kmu/com/navigationdrawerrrrr/PedometerActivity.java
+++ HealthMate/app/src/main/java/my/kmu/com/navigationdrawerrrrr/PedometerActivity.java
@@ -1,16 +1,95 @@
 package my.kmu.com.navigationdrawerrrrr;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.view.Window;
+
 
 /**
  * Created by eomhyeong-geun on 2016. 11. 20..
  */
 
-public class PedometerActivity extends Activity {
-    @Override
+public class PedometerActivity extends Activity implements SensorEventListener {
+    public static int cnt = 0;
+
+    private TextView tv_count, tv_remain;
+
+    private long lastTime;
+    private float speed;
+    private float lastX;
+    private float lastY;
+    private float lastZ;
+    private float x, y, z;
+
+    private static final int SHAKE_THRESHOLD = 800;
+    private static final int DATA_X = SensorManager.DATA_X;
+    private static final int DATA_Y = SensorManager.DATA_Y;
+    private static final int DATA_Z = SensorManager.DATA_Z;
+
+    private SensorManager sensorManager;
+    private Sensor accelerormeterSensor;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedometer);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerormeterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        tv_count = (TextView)findViewById(R.id.tv_count);
+        tv_remain= (TextView)findViewById(R.id.tv_remain);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (accelerormeterSensor != null)
+            sensorManager.registerListener(this, accelerormeterSensor,
+                    SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (sensorManager != null)
+            sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long currentTime = System.currentTimeMillis();
+            long gabOfTime = (currentTime - lastTime);
+            if (gabOfTime > 100) {
+                lastTime = currentTime;
+                x = event.values[SensorManager.DATA_X];
+                y = event.values[SensorManager.DATA_Y];
+                z = event.values[SensorManager.DATA_Z];
+
+                speed = Math.abs(x + y + z - lastX - lastY - lastZ) / gabOfTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    tv_count.setText("" + (++cnt) + "걸음");
+                    tv_remain.setText("" +(9999-cnt) + "걸음");
+
+                }
+
+                lastX = event.values[DATA_X];
+                lastY = event.values[DATA_Y];
+                lastZ = event.values[DATA_Z];
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
